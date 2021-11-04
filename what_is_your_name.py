cc0ccd9d078bd8f7fdff274616d92d1bc1b5779c
gowthami-akarsha <aakarsha0340@gmail.com>
2021-11-04 10:38:54 +0530

    what is your name

---
+++ what_is_your_name.py
@@ -0,0 +1,9 @@
+# What's Your Name?
+def print_full_name(first, last):
+    # Write your code here
+
+  if __name__ == '__main__': 
+    first_name =int("raw_input"())
+    last_name =int("raw_input"())
+    print_full_name(first_name, last_name)
+

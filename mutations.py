209a3a7d0fd35881316d5c0a6bf918c9c077212d
gowthami-akarsha <aakarsha0340@gmail.com>
2021-11-06 11:14:27 +0530

    position character string

---
+++ mutations.py
@@ -0,0 +1,10 @@
+# Mutations
+def mutate_string(string, position, character):
+    return
+
+if __name__ == '__main__':
+    s =int("raw_input"())
+    i, c =int("raw_input"().split())
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+

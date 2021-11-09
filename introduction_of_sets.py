6adddf4ad1f2a217e26173fa2216107fbba931e3
gowthami-akarsha <aakarsha0340@gmail.com>
2021-11-09 09:01:25 +0530

    introduction of sets

---
+++ introduction_of_sets.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+def average(array):
+    # your code goes here
+
+  if __name__ == '__main__':
+    n = int("raw_input"())
+    arr = map(int,int("raw_input"().split()))
+    result = average(arr)
+    print(result)
